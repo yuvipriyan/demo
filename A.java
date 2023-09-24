72899cc15ac10c0453160f9fb034cfec29432072
yuvarajkcp <yuvakcp@gmail.com>
2023-09-24 00:17:22 -0100

    azhagumittai

---
+++ A.java
@@ -0,0 +1,10 @@
+class A
+{
+	public static void main(String[] args)
+	{
+		System.out.println("How are you manju");
+		System.out.println("How are you manju");
+		System.out.println("How are you manju");
+		System.out.println("How are you manju");
+	}
+}
